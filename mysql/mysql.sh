c5d17b55476aab32e1d1d305c73cecadd56dea10
wyy0117 <wyy699179364>
2020-06-30 13:40:18 +0800

    格式化

--- mysql/mysql.sh
+++ mysql/mysql.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
-base_path=/Volumes/home/docker/data/mysql5.7
-port=3306
-container_name=mysql
+base_path=/data/bimgroupdev/mysql5.7
+port=3309
+container_name=bimdevmysql
 root_password=root
-
-
 
 mkdir -p $base_path/{conf,dbdata}
 cp ./config-file.cnf $base_path/conf/.
 
-docker run -itd --restart=always --name $container_name -p $port:3306 -v $base_path/conf:/etc/mysql/conf.d -v $base_path/dbdata:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$root_password mysql:5.7
+docker run -itd --restart=always --name $container_name \
+  -p $port:3306 \
+  -v $base_path/conf:/etc/mysql/conf.d \
+  -v $base_path/dbdata:/var/lib/mysql \
+  -e MYSQL_ROOT_PASSWORD=$root_password \
+  mysql:5.7
