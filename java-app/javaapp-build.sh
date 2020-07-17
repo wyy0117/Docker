5b2aeb8af4789fb0e053a3119c0450fe6640fa94
wyy0117 <wyy699179364>
2020-07-17 15:23:58 +0800

    优化javaapp

--- java-app/javaapp-build.sh
+++ java-app/javaapp-build.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
-base_path=~/cache/temp/demo/target
+base_path=/Users/wyy/cache/temp/demo/target
+image_name=demo
 container_name=demo
 port=8080
 spring_profile_active=production
 
 mkdir -p $base_path
 
+docker build -t $image_name .
+
 docker run -itd --name $container_name --restart=always \
   -v $base_path:/app \
   -e SPRING_PROFILES_ACTIVE=$spring_profile_active \
   -p $port:$port \
-  wyy349093330/javaapp:jre8
+  $image_name
---
+++ java-app/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+base_path=/app
+cd $base_path
+java -Djava.security.egd=file:/dev/./urandom -jar *.*ar
