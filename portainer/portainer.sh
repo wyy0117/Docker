4e4971bd1f63eef44e900016b67b90979d332c12
wyy0117 <wyy699179364>
2020-07-21 18:42:30 +0800

    添加portainer对agent的支持

--- portainer/portainer.sh
+++ portainer/portainer.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-base_path=~/docker/data/portainer
+base_path=/Users/wyy/docker/portainer
 container_name=portainer
-port=9000
+port1=8000
+port2=9000
 
 mkdir -p $base_path/data
-docker run -itd -p $port:9000 --name $container_name --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v $base_path/data:/data portainer/portainer
+docker run -itd -p $port1:8000 -p $port2:9000 --name $container_name --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v $base_path/data:/data portainer/portainer
