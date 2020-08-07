525e1cd50b7ca13900a10572cc31945b1b867479
王玉园 <wyy699179364>
2020-08-07 15:11:05 +0800

    优化portainer

--- portainer/portainer.sh
+++ portainer/portainer.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-base_path=/Users/wyy/docker/portainer
+base_path=~/docker/portainer
 container_name=portainer
-port1=8000
-port2=9000
+#edge agent port
+tunnelport=8001
+#浏览器中查看页面的端口号
+uiport=9001
 
 mkdir -p $base_path/data
-docker run -itd -p $port1:8000 -p $port2:9000 --name $container_name --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v $base_path/data:/data portainer/portainer
+docker run -itd -p $tunnelport:$tunnelport -p $uiport:9000 --name $container_name --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v $base_path/data:/data portainer/portainer --tunnel-port=$tunnelport
