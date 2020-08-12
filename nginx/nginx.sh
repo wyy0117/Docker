efe6c7bad475705f553f097d390b2e23f953147d
王玉园 <wyy699179364>
2020-08-12 19:42:54 +0800

    优化nginx的log路径

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -4,6 +4,11 @@ base_path=~/docker/data/nginx
 port=80
 container_name=nginx
 
-mkdir -p $base_path/conf
+mkdir -p $base_path/{conf,log}
 cp ./nginx.conf $base_path/conf/.
-docker run -itd --restart=always --name=$container_name -p $port:80 -v $base_path/conf/nginx.conf:/etc/nginx/nginx.conf nginx
+docker run -itd --restart=always \
+  --name=$container_name \
+  -p $port:80 \
+  -v $base_path/conf/nginx.conf:/etc/nginx/nginx.conf \
+  -v $base_path/log:/var/log/nginx
+nginx
