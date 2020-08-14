e7c630c69989975384d7ec7e71967b467bf97f9f
王玉园 <wyy699179364>
2020-08-14 14:45:46 +0800

    优化rabbitmq

--- rabbitmq/rabbitmq.sh
+++ rabbitmq/rabbitmq.sh
@@ -8,4 +8,11 @@ password=admin
 
 mkdir -p $base_path
 
-docker run -itd --restart=always --name $container_name -v $base_path:/var/lib/rabbitmq -p $port1:5672 -p $port2:15672 -e RABBITMQ_DEFAULT_USER=$username -e RABBITMQ_DEFAULT_PASS=$password rabbitmq:3-management
+docker run -itd --restart=always \
+  --name $container_name \
+  -v $base_path:/var/lib/rabbitmq \
+  -p $port1:5672 \
+  -p $port2:15672 \
+  -e RABBITMQ_DEFAULT_USER=$username \
+  -e RABBITMQ_DEFAULT_PASS=$password \
+  rabbitmq:3-management
