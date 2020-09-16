ac544a310e0272a88a9aa75f59b2679bad7f908c
wangyuyuan <wangyuyuan@suitbim.com>
2020-09-16 20:17:16 +0800

    optimize httpserver

--- httpserver/httpserver.sh
+++ httpserver/httpserver.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-base_path=~/docker/data/httpserver
+base_path=~/cache/docker/data/httpserver
 container_name=httpserver
 port=80
 
-mkdir -p $base_path/{conf,html}
+mkdir -p $base_path/{conf,html,log}
 cp ./nginx.conf $base_path/conf/.
 
-docker run -itd --restart=always --name $container_name -p $port:80 -v $base_path/conf/nginx.conf:/etc/nginx/nginx.conf -v $base_path/html:/usr/share/nginx/html nginx
+docker run -itd --restart=always \
+  --name $container_name \
+  -p $port:80 \
+  -v $base_path/conf/nginx.conf:/etc/nginx/nginx.conf \
+  -v $base_path/html:/usr/share/nginx/html \
+  -v $base_path/log:/var/log/nginx \
+  nginx
